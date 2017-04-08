3d16eb44a115001a14504a97574e6d03d23855c5
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2017-04-08 09:04:34 -0700

    add proper error messages as well as support for all entry fields

--- js/scripts.js
+++ js/scripts.js
@@ -57,12 +57,28 @@
 //   }
 // }
 
+function resetFields() {
+  $("input#activity").val("");
+  $("input#address").val("");
+  $("input#time").val("");
+  $("input#description").val("");
+}
+
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
     center: {lat: 44.049301, lng: -123.095047}
   });
+
+
   var geocoder = new google.maps.Geocoder();
+//       var inputtedActivity = $("input#activity").val();
+//
+//   var contentString = "Type of Activity: " + inputtedActivity;
+//
+//   var infowindow = new google.maps.InfoWindow({
+//     content: contentString
+// });
 
   document.getElementById('submit').addEventListener('click', function() {
     geocodeAddress(geocoder, map);
@@ -73,15 +89,38 @@ function initMap() {
 function geocodeAddress(geocoder, resultsMap) {
   var address = document.getElementById('address').value;
   geocoder.geocode({'address': address}, function(results, status) {
-    if (status === 'OK') {
+    var activity = document.getElementById("activity");
+    var inputtedActivity = activity.options[activity.selectedIndex].text;
+    var inputtedAddress = $("input#address").val();
+    var inputtedTime = $("input#time").val();
+    var inputtedDescription = $("input#description").val();
+    if (status === 'OK' && inputtedAddress != "" && inputtedActivity != "Select a Category..." && inputtedTime != "") {
+      console.log(inputtedActivity, inputtedAddress, inputtedTime, inputtedDescription);
       resultsMap.setCenter(results[0].geometry.location);
       var marker = new google.maps.Marker({
         map: resultsMap,
         position: results[0].geometry.location
       });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
+
+      var contentString = "Incident Description: " + inputtedDescription;
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+      marker.addListener('click', function() {
+         infowindow.open(map, marker);
+       });
+
+    } else if (status === 'ZERO_RESULTS' && inputtedActivity != "Select a Category..." && inputtedTime != "") {
+       {
+      alert("Enter in standard addresses (e.g. 999 Willamette st) or location names (e.g. Eugene Public Library).");
     }
+  } else if (inputtedActivity === "Select a Category..." || inputtedTime === "" || inputtedAddress === "") {
+    console.log(inputtedActivity, inputtedAddress, inputtedTime, inputtedDescription);
+    alert ("You must fill out all fields!");
+  } else {
+    alert("Your address appears to be blank, try again.");
+  }
   });
 }
 
@@ -95,5 +134,8 @@ $(document).ready(function() {
     var inputtedAddress = $("input#address").val();
     var inputtedTime = $("input#time").val();
     var inputtedDescription = $("input#description").val();
+
+    resetFields()
   });
+
 });
